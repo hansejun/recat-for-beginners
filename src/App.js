f3339f34107a17ee58fca0f7d3e87e6a2e5a6e4a
hansejun <tpwnszja2@gmail.com>
2022-03-18 16:17:46 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/movies/:id`}
-          element={<Detail />}
-        />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes path={"/"}>
+        <Route index element={<Home />} />
+        <Route path={`movies/:id`} element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
